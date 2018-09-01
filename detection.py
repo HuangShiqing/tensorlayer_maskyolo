9c35f326453b1ca36afc8117cee5a555560e646a
黄世青 <hsqyc@163.com>
2018-09-01 14:39:50 +0800

    add a detection the result is better than i image

--- detection.py
+++ detection.py
@@ -11,13 +11,10 @@ from net import Gb_all_layer_out, ResLayer, RouteLayer, upsample, conv2d_unit, d
 
 # net_out = [tf.zeros(shape=(1, 52, 52, 3, 85)), tf.zeros(shape=(1, 26, 26, 3, 85)), tf.zeros(shape=(1, 13, 13, 3, 85))]
 checkpoint_dir = './ckpt/'
-ckpt_name = 'ep000-step46000-loss2.157-46000'
-label = ['knot']
-anchors = tf.constant([125, 311, 127, 192, 212, 378, 273, 178, 324, 490, 362, 865, 404, 292, 513, 505, 639, 727],
-                      # [10, 13, 16, 30, 33, 23, 30, 61, 62, 45, 59, 119, 116, 90, 156, 198, 373, 326],
-                      dtype='float', shape=[1, 1, 1, 9, 2])
-n_class = len(label)
+ckpt_name = 'ep373-step66866-loss1389.557'
+# label = ['knot']
 
+n = 2
 input_pb = tf.placeholder(tf.float32, [None, 416, 416, 3])
 net = InputLayer(input_pb, name='input')
 net = conv2d_unit(net, filters=32, kernels=3, strides=1, bn=True, name='0')
@@ -101,7 +98,7 @@ net = conv2d_unit(net, filters=512, kernels=1, strides=1, bn=True, name='77')
 net = conv2d_unit(net, filters=1024, kernels=3, strides=1, bn=True, name='78')
 net = conv2d_unit(net, filters=512, kernels=1, strides=1, bn=True, name='79')
 net = conv2d_unit(net, filters=1024, kernels=3, strides=1, bn=True, name='80')
-net = conv2d_unit(net, filters=3 * (5 + n_class), kernels=1, strides=1, act='liner', bn=False, name='81')
+net = conv2d_unit(net, filters=n, kernels=1, strides=1, act='liner', bn=False, name='81')
 detection(net, '82')
 net = RouteLayer(net, [79], name='83')
 net = conv2d_unit(net, filters=256, kernels=1, strides=1, bn=True, name='84')
@@ -113,7 +110,7 @@ net = conv2d_unit(net, filters=256, kernels=1, strides=1, bn=True, name='89')
 net = conv2d_unit(net, filters=512, kernels=3, strides=1, bn=True, name='90')
 net = conv2d_unit(net, filters=256, kernels=1, strides=1, bn=True, name='91')
 net = conv2d_unit(net, filters=512, kernels=3, strides=1, bn=True, name='92')
-net = conv2d_unit(net, filters=3 * (5 + n_class), kernels=1, strides=1, act='liner', bn=False, name='93')
+net = conv2d_unit(net, filters=n, kernels=1, strides=1, act='liner', bn=False, name='93')
 detection(net, '94')
 net = RouteLayer(net, [91], name='95')
 net = conv2d_unit(net, filters=128, kernels=1, strides=1, bn=True, name='96')
@@ -125,9 +122,9 @@ net = conv2d_unit(net, filters=128, kernels=1, strides=1, bn=True, name='101')
 net = conv2d_unit(net, filters=256, kernels=3, strides=1, bn=True, name='102')
 net = conv2d_unit(net, filters=128, kernels=1, strides=1, bn=True, name='103')
 net = conv2d_unit(net, filters=256, kernels=3, strides=1, bn=True, name='104')
-net = conv2d_unit(net, filters=3 * (5 + n_class), kernels=1, strides=1, act='liner', bn=False, name='105')
+net = conv2d_unit(net, filters=n, kernels=1, strides=1, act='liner', bn=False, name='105')
 detection(net, '106')
-net_out = [Gb_all_layer_out[106], Gb_all_layer_out[94], Gb_all_layer_out[82]]
+net_out = Gb_all_layer_out[106]
 
 # 读取ckpt里保存的参数
 sess = tf.InteractiveSession()
@@ -139,145 +136,23 @@ if tf.train.get_checkpoint_state(checkpoint_dir):  # 确认是否存在
 else:
     print("ckpt文件不存在")
 
-# tensor = tf.global_variables('layer_0_conv')
-# b = sess.run(tensor)
-# c = sess.run(net_out, feed_dict={input_pb: image_data})
-if not os.path.exists('out'):
-    os.mkdir('out')
-file_name = input('Input image filedir:')
-img_path = os.listdir(file_name)
-for path in tqdm(img_path):
-    abs_path = file_name + path
-    img = cv2.imread(abs_path)
-# while True:
-#     file_name = input('Input image filename:')
-#     img = cv2.imread(file_name)
-    img = img[:, :, ::-1]  # RGB image
-    img_shape = img.shape[0:2][::-1]
+from data import resize_img, visualization
 
-    _scale = min(416 / img_shape[0], 416 / img_shape[1])
-    _new_shape = (int(img_shape[0] * _scale), int(img_shape[1] * _scale))
-    im_sized = cv2.resize(img, _new_shape)
-    im_sized = np.pad(im_sized,
-                      (
-                          (int((416 - _new_shape[1]) / 2), 416 - _new_shape[1] - int((416 - _new_shape[1]) / 2)),
-                          (int((416 - _new_shape[0]) / 2), 416 - _new_shape[0] - int((416 - _new_shape[0]) / 2)),
-                          (0, 0)
-                      ),
-                      mode='constant')
-    image_data = np.array(im_sized, dtype='float32')
-    image_data /= 255.
-    image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+img_path = 'C:/Users/john/Desktop/timg.jpg'#'D:/DeepLearning/data2/VOCdevkit/VOC2012/JPEGImages/2007_000170.jpg'
+origin_img = cv2.imread(img_path)
+origin_img = origin_img[:, :, :: -1]
+origin_img = resize_img(origin_img)
+img = origin_img / 255.
+img = np.expand_dims(img, axis=0)
 
-    input_shape = tf.cast(tf.shape(net_out[2])[1:3] * 32, dtype='float32')[::-1]  # hw
-    image_shape = tf.cast(img_shape, dtype='float32')[::-1]  # hw
-    new_shape = tf.round(image_shape * tf.reduce_min(input_shape / image_shape))
-    offset = (input_shape - new_shape) / 2. / input_shape
-    scale = input_shape / new_shape
+adjusted_out = tf.sigmoid(net_out)
+out = sess.run(adjusted_out, feed_dict={input_pb: img})
 
-    # with tf.Session() as sess:
-    #     a = sess.run(scale)
+out = out[0, :, :, 1]
+out = out * 21
+out = np.floor(out)
+out = out.astype(np.uint8)
+cv2.imwrite('out1.bmp',out)
+visualization(origin_img, out)
 
-    boxes = list()
-    box_scores = list()
-
-    cellbase_x = tf.to_float(tf.reshape(tf.tile(tf.range(52), [52]), (1, 52, 52, 1, 1)))
-    cellbase_y = tf.transpose(cellbase_x, (0, 2, 1, 3, 4))
-    cellbase_grid = tf.tile(tf.concat([cellbase_x, cellbase_y], -1), [1, 1, 1, 3, 1])
-    # classes = list()
-    for i in range(3):  # 52 26 13
-        anchor = anchors[..., 3 * i:3 * (i + 1), :]
-        # feats = model.output[i]
-        feats = net_out[i]
-
-        grid_w = tf.shape(feats)[1]  # 13
-        grid_h = tf.shape(feats)[2]  # 13
-        grid_factor = tf.reshape(tf.cast([grid_w, grid_h], tf.float32), [1, 1, 1, 1, 2])
-
-        feats = tf.reshape(feats, [-1, grid_w, grid_h, 3, n_class + 5])
-
-        # Adjust preditions to each spatial grid point and anchor size.
-        box_xy = (tf.sigmoid(feats[..., :2]) + cellbase_grid[:, :grid_w, :grid_h, :, :]) / tf.cast(grid_factor[::-1],
-                                                                                                   'float32')
-        box_wh = tf.exp(feats[..., 2:4]) * anchor / tf.cast(input_shape[::-1], 'float32')
-        box_confidence = tf.sigmoid(feats[..., 4:5])
-        box_class_probs = tf.sigmoid(feats[..., 5:])
-
-        box_yx = box_xy[..., ::-1]
-        box_hw = box_wh[..., ::-1]
-        box_yx = (box_yx - offset) * scale
-        box_hw *= scale
-        box_mins = box_yx - (box_hw / 2.)
-        box_maxes = box_yx + (box_hw / 2.)
-        _boxes = tf.concat([
-            box_mins[..., 0:1],  # y_min
-            box_mins[..., 1:2],  # x_min
-            box_maxes[..., 0:1],  # y_max
-            box_maxes[..., 1:2]  # x_max
-        ], axis=-1)
-
-        # Scale boxes back to original image shape.
-        _boxes *= tf.concat([tf.cast(image_shape, 'float32'), tf.cast(image_shape, 'float32')], axis=-1)
-        _boxes = tf.reshape(_boxes, [-1, 4])
-
-        _box_scores = box_confidence * box_class_probs
-        _box_scores = tf.reshape(_box_scores, [-1, n_class])
-        boxes.append(_boxes)
-        box_scores.append(_box_scores)
-    boxes = tf.concat(boxes, axis=0)
-    box_scores = tf.concat(box_scores, axis=0)
-
-    mask = box_scores >= 0.3
-    max_num_boxes = tf.constant(20, dtype='int32')
-
-    boxes_ = []
-    scores_ = []
-    classes_ = []
-    for c in range(n_class):
-        class_boxes = tf.boolean_mask(boxes, mask[:, c])
-        class_box_scores = tf.boolean_mask(box_scores[:, c], mask[:, c])
-        nms_index = tf.image.non_max_suppression(
-            class_boxes, class_box_scores, max_num_boxes, iou_threshold=0.5)
-        class_boxes = tf.gather(class_boxes, nms_index)
-        class_box_scores = tf.gather(class_box_scores, nms_index)
-        classes = tf.ones_like(class_box_scores, 'int32') * c
-        boxes_.append(class_boxes)
-        scores_.append(class_box_scores)
-        classes_.append(classes)
-    boxes_ = tf.concat(boxes_, axis=0)
-    scores_ = tf.concat(scores_, axis=0)
-    classes_ = tf.concat(classes_, axis=0)
-
-    b, s, c = sess.run([boxes_, scores_, classes_], feed_dict={input_pb: image_data})
-
-    # plt.cla()
-    # plt.imshow(img)
-    # for i, obj in enumerate(b):
-    #     x1 = obj[1]
-    #     x2 = obj[3]
-    #     y1 = obj[0]
-    #     y2 = obj[2]
-    #
-    #     # TODO: change the color of text
-    #     plt.text(x1, y1 - 10, round(s[i], 2))
-    #     plt.text(x2 - 30, y1 - 10, label[c[i]])
-    #     plt.hlines(y1, x1, x2, colors='red')
-    #     plt.hlines(y2, x1, x2, colors='red')
-    #     plt.vlines(x1, y1, y2, colors='red')
-    #     plt.vlines(x2, y1, y2, colors='red')
-    # plt.show()
-    img = img[:, :, ::-1]
-    file = open("./out/" + path.rstrip('.jpg') + '.txt', 'w')
-    for i, obj in enumerate(b):
-        cv2.rectangle(img, (obj[1], obj[0]), (obj[3], obj[2]), (0, 0, 255), 3)
-        cv2.putText(img, str(round(s[i], 2)), (int(obj[1]), int(obj[0]) - 10), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255),
-                    3)
-        cv2.putText(img, str(label[c[i]]), (int(obj[3]) - 100, int(obj[0]) - 10), cv2.FONT_HERSHEY_COMPLEX, 2,
-                    (0, 0, 255), 3)
-
-        file.write('{0} {1} '.format(label[c[i]], s[i]))
-        file.write('{0} {1} {2} {3}'.format(obj[1], obj[0], obj[3], obj[2]))
-        file.write('\n')
-    file.close()
-    cv2.imwrite("./out/" + path, img)
-    # cv2.imwrite("1.jpg", img)
+exit()
--- train.py
+++ train.py
@@ -8,14 +8,14 @@ from model import infenence
 import time
 import os
 
+
 def yolo_loss(y_pred, y_true):
     # out_c = tf.sigmoid(y_pred[..., 0])  # tf.expand_dims(tf.sigmoid(y_pred[..., 0]), axis=-1)
     # out_class = tf.sigmoid(y_pred[..., 1])  # * len(Gb_labels)  # tf.expand_dims(tf.sigmoid(y_pred[..., 1]))
 
     object_mask = tf.where(y_true != 0, tf.ones_like(y_true), tf.zeros_like(y_true))
     noobject_mask = 1 - object_mask
-    # object_mask = y_true != 0
-    # noobject_mask = y_true == 0
+
     true_c = object_mask
     true_class = y_true / (len(Gb_labels))
 
@@ -40,99 +40,6 @@ def yolo_loss(y_pred, y_true):
     # loss_sum = tf.Print(loss_sum, [loss_c, loss_class])
     # loss_sum = tf.Print(loss_sum, [loss_c, loss_class_obj, loss_class_noobj])
     return loss_sum
-
-
-# def yolo3_loss(y_pred, y_true):
-#     anchors = tf.constant(Gb_anchors, dtype='float', shape=[1, 1, 1, 9, 2])
-#     C = len(Gb_label)
-#     ignore_thresh = 0.5
-#     batch_size = Gb_batch_size
-#     with tf.variable_scope('loss') as scope:
-#         cellbase_x = tf.to_float(tf.reshape(tf.tile(tf.range(52), [52]), (1, 52, 52, 1, 1)))
-#         cellbase_y = tf.transpose(cellbase_x, (0, 2, 1, 3, 4))
-#         cellbase_grid = tf.tile(tf.concat([cellbase_x, cellbase_y], -1), [batch_size, 1, 1, 3, 1])
-#
-#         img_w = 416
-#         img_h = 416
-#         img_factor = tf.reshape(tf.cast([img_w, img_h], tf.float32), [1, 1, 1, 1, 2])
-#
-#         loss = 0
-#         sum_loss_xy = 0
-#         sum_loss_wh = 0
-#         sum_loss_c = 0
-#         sum_loss_class = 0
-#         for i in range(3):
-#             # TODO:adjust the order of the anchor
-#             anchor = anchors[..., 3 * i:3 * (i + 1), :]
-#             object_mask = y_true[i][..., 4:5]
-#
-#             grid_w = tf.shape(y_pred[i])[1]  # 13
-#             grid_h = tf.shape(y_pred[i])[2]  # 13
-#             grid_factor = tf.reshape(tf.cast([grid_w, grid_h], tf.float32), [1, 1, 1, 1, 2])
-#
-#             net_out_reshape = tf.reshape(y_pred[i], [-1, grid_w, grid_h, 3, (4 + 1 + C)])
-#             adjusted_out_xy = (cellbase_grid[:, :grid_w, :grid_h, :, :] + tf.sigmoid(
-#                 net_out_reshape[..., :2])) / grid_factor
-#             adjusted_out_wh = tf.exp(net_out_reshape[..., 2:4]) * anchor / img_factor
-#             adjusted_out_c = tf.expand_dims(tf.sigmoid(net_out_reshape[..., 4]), axis=-1)
-#             adjusted_out_class = tf.sigmoid(net_out_reshape[..., 5:])
-#             adjusted_net_out = tf.concat([adjusted_out_xy, adjusted_out_wh, adjusted_out_c, adjusted_out_class],
-#                                          axis=-1)
-#             pred_boxes = tf.expand_dims(adjusted_net_out[..., 0:4], 4)
-#
-#             adjusted_true_xy = y_true[i][..., :2] * grid_factor - cellbase_grid[:, :grid_w, :grid_h, :, :]
-#             adjusted_true_wh = tf.log(y_true[i][..., 2:4] / anchor * img_factor + 1e-9)  # 1e-9 just avoid log(0) = -inf
-#
-#             adjusted_true_c = y_true[i][..., 4:5]
-#             adjusted_true_class = y_true[i][..., 5:]
-#
-#             # TODO i don't like for loop
-#             # origin_boxes = list()
-#             ignore_masks = list()
-#             for k in range(batch_size):
-#                 origin_box = tf.boolean_mask(y_true[i][k, ..., :4], tf.cast(y_true[i][k, ..., 4], dtype=bool))
-#                 origin_box = tf.tile(tf.reshape(origin_box, shape=[1, 1, 1, -1, 4]), [grid_w, grid_h, 3, 1, 1])
-#                 iou_scores = iou(pred_boxes[k], origin_box)
-#                 best_ious = tf.reduce_max(iou_scores, axis=-1)
-#                 ignore_mask = tf.expand_dims(tf.to_float(best_ious < ignore_thresh), -1)
-#                 ignore_masks.append(ignore_mask)
-#             ignore_masks = tf.stack(ignore_masks)
-#             # origin_boxes.append(origin_box)
-#             # # origin_boxes = tf.stack(origin_boxes)
-#             # iou_scores = iou(pred_boxes, origin_boxes)
-#             # best_ious = tf.reduce_max(iou_scores, axis=-1)
-#             # ignore_mask = tf.expand_dims(tf.to_float(best_ious < ignore_thresh), 4)
-#
-#             xywh_scale = 2 - y_true[i][..., 2:3] * y_true[i][..., 3:4]
-#
-#             loss_xy = tf.reduce_sum(
-#                 object_mask * xywh_scale * tf.nn.sigmoid_cross_entropy_with_logits(logits=net_out_reshape[..., :2],
-#                                                                                    labels=adjusted_true_xy)) / batch_size
-#             loss_wh = tf.reduce_sum(
-#                 object_mask * xywh_scale * 0.5 * tf.square(net_out_reshape[..., 2:4] - adjusted_true_wh)) / batch_size
-#             loss_c = tf.reduce_sum(
-#                 object_mask * tf.nn.sigmoid_cross_entropy_with_logits(logits=net_out_reshape[..., 4:5],
-#                                                                       labels=adjusted_true_c) + (
-#                         1 - object_mask) * tf.nn.sigmoid_cross_entropy_with_logits(logits=net_out_reshape[..., 4:5],
-#                                                                                    labels=adjusted_true_c) * ignore_masks) / batch_size
-#             loss_class = tf.reduce_sum(
-#                 object_mask * tf.nn.sigmoid_cross_entropy_with_logits(logits=net_out_reshape[..., 5:],
-#                                                                       labels=adjusted_true_class)) / batch_size
-#
-#             sum_loss_xy += loss_xy
-#             sum_loss_wh += loss_wh
-#             sum_loss_c += loss_c
-#             sum_loss_class += loss_class
-#             loss += loss_xy + loss_wh + loss_c + loss_class
-#
-#         tf.summary.scalar('/loss', loss)
-#         tf.summary.scalar('/loss_xy', sum_loss_xy)
-#         tf.summary.scalar('/loss_wh', sum_loss_wh)
-#         tf.summary.scalar('/loss_c', sum_loss_c)
-#         tf.summary.scalar('/loss_class', sum_loss_class)
-#         # loss = tf.Print(loss, [sum_loss_xy, sum_loss_wh, sum_loss_c, sum_loss_class])
-#
-#     return loss
 
 
 def training(loss, learning_rate):
@@ -197,17 +104,9 @@ def main():
                 step_epoch += 1
                 start_time = time.time()
 
-                loss, _ = sess.run([loss_op, train_op],
-                                   feed_dict={input_pb: origin_img_sizeds, y_true_pb: segment_datas})
-
-                # 50倍打印频率的整数倍step，写进TensorBorder一次
-                if step + 1 == 1 or (step + 1) % 1 == 0:
-                    summary_str = sess.run(summary_op,
-                                           feed_dict={input_pb: origin_img_sizeds, y_true_pb: segment_datas})
-                    # , feed_dict={input_pb: img, y_true_pb_1: lable_box[0],
-                    #               y_true_pb_2: lable_box[1],
-                    #               y_true_pb_3: lable_box[2]})
-                    train_writer.add_summary(summary_str, step)
+                summary_str, loss, _ = sess.run([summary_op, loss_op, train_op],
+                                                feed_dict={input_pb: origin_img_sizeds, y_true_pb: segment_datas})
+                train_writer.add_summary(summary_str, step)
 
                 # 每step打印一次该step的loss
                 print("Loss %fs  : Epoch %d  %d/%d: Step %d  took %fs" % (
@@ -221,9 +120,9 @@ def main():
 
                 if step % save_frequency == 0:
                     if step != save_frequency:
-                        os.remove(temp+'.data-00000-of-00001')
-                        os.remove(temp + '.index')
-                        os.remove(temp + '.meta')
+                        os.remove(final_dir + temp + '.data-00000-of-00001')
+                        os.remove(final_dir + temp + '.index')
+                        os.remove(final_dir + temp + '.meta')
 
                     print("Save model " + "!" * 10)
                     save_path = saver.save(sess,
